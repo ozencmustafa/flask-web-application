234ca335adac426718e302f50030c9c12dfac72c
ozencmustafa <ozencmustafa@hotmail.com>
2022-02-21 22:22:33 +0100

    flask app v1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)   # initialization of my flask application
+
+
+# render a html template instead of returning a html codes in return.
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
